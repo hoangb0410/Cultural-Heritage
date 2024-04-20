18ac9b2eef5b297144a248522f96fa07fd7456fd
Hoàng Cao <hoangb0410@192.168.17.101>
2024-04-20 12:29:25 +0700

    update user

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, min: 6, max: 20, unique: true },
-    fullname: {type: String, required: true, min: 5, max: 30},
+    fullname: {type: String, min: 5, max: 30},
     email: { type: String, required: true, max: 50, unique: true },
     password: { type: String, required: true, min: 6 },
     isAdmin: { type: Boolean, default: false},
